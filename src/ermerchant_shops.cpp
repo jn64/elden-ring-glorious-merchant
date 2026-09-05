5fc218f743dd3c4228530c050c9e7f17d2351076
Tom Clark <the.thomas.j.clark@gmail.com>
2026-09-05 16:57:38 -0400

    Sane int types

--- src/ermerchant_shops.cpp
+++ src/ermerchant_shops.cpp
@@ -8,6 +8,7 @@
 #include "ermerchant_shops.hpp"
 
 #include <array>
+#include <cstdint>
 #include <set>
 #include <vector>
 
@@ -30,64 +31,64 @@
 static const std::wstring cut_content_prefix = L"[ERROR]";
 static const std::wstring reforged_cut_spirit_summon_prefix = L"Depleted ";
 
-static constexpr unsigned char lot_item_category_goods = 1;
+static constexpr uint8_t lot_item_category_goods = 1;
 
-static constexpr unsigned char cost_type_lost_ashes_of_war = 4;
+static constexpr uint8_t cost_type_lost_ashes_of_war = 4;
 
-static constexpr unsigned char equip_type_weapon = 0;
-static constexpr unsigned char equip_type_protector = 1;
-static constexpr unsigned char equip_type_accessory = 2;
-static constexpr unsigned char equip_type_goods = 3;
-static constexpr unsigned char equip_type_gem = 4;
+static constexpr uint8_t equip_type_weapon = 0;
+static constexpr uint8_t equip_type_protector = 1;
+static constexpr uint8_t equip_type_accessory = 2;
+static constexpr uint8_t equip_type_goods = 3;
+static constexpr uint8_t equip_type_gem = 4;
 
-static constexpr unsigned char weapon_type_arrow = 81;
-static constexpr unsigned char weapon_type_greatarrow = 83;
-static constexpr unsigned char weapon_type_bolt = 85;
-static constexpr unsigned char weapon_type_ballista_bolt = 86;
+static constexpr uint8_t weapon_type_arrow = 81;
+static constexpr uint8_t weapon_type_greatarrow = 83;
+static constexpr uint8_t weapon_type_bolt = 85;
+static constexpr uint8_t weapon_type_ballista_bolt = 86;
 
-static constexpr long long weapon_unarmed_id = 110000;
+static constexpr int64_t weapon_unarmed_id = 110000;
 
-static constexpr unsigned char protector_category_head = 0;
-static constexpr unsigned char protector_category_chest = 1;
-static constexpr unsigned char protector_category_arms = 2;
-static constexpr unsigned char protector_category_legs = 3;
+static constexpr uint8_t protector_category_head = 0;
+static constexpr uint8_t protector_category_chest = 1;
+static constexpr uint8_t protector_category_arms = 2;
+static constexpr uint8_t protector_category_legs = 3;
 
-static constexpr long long protector_bare_head_id = 10000;
-static constexpr long long protector_bare_chest_id = 10100;
-static constexpr long long protector_bare_arms_id = 10200;
-static constexpr long long protector_bare_legs_id = 10300;
+static constexpr int64_t protector_bare_head_id = 10000;
+static constexpr int64_t protector_bare_chest_id = 10100;
+static constexpr int64_t protector_bare_arms_id = 10200;
+static constexpr int64_t protector_bare_legs_id = 10300;
 
-static constexpr long long goods_golden_seed_id = 10010;
-static constexpr long long goods_sacred_tear_id = 10020;
+static constexpr int64_t goods_golden_seed_id = 10010;
+static constexpr int64_t goods_sacred_tear_id = 10020;
 
-static constexpr unsigned char goods_type_normal_item = 0;
-static constexpr unsigned char goods_type_key_item = 1;
-static constexpr unsigned char goods_type_crafting_material = 2;
-static constexpr unsigned char goods_type_remembrance = 3;
-static constexpr unsigned char goods_type_sorcery = 5;
-static constexpr unsigned char goods_type_spirit_summon_lesser = 7;
-static constexpr unsigned char goods_type_spirit_summon_greater = 8;
-static constexpr unsigned char goods_type_wondrous_physick = 9;
-static constexpr unsigned char goods_type_wondrous_physick_tear = 10;
-static constexpr unsigned char goods_type_regenerative_material = 11;
-static constexpr unsigned char goods_type_info_item = 12;
-static constexpr unsigned char goods_type_reinforcement_material = 14;
-static constexpr unsigned char goods_type_great_rune = 15;
-static constexpr unsigned char goods_type_incantation = 16;
-static constexpr unsigned char goods_type_self_buff_sorcery = 17;
-static constexpr unsigned char goods_type_self_buff_incantation = 18;
+static constexpr uint8_t goods_type_normal_item = 0;
+static constexpr uint8_t goods_type_key_item = 1;
+static constexpr uint8_t goods_type_crafting_material = 2;
+static constexpr uint8_t goods_type_remembrance = 3;
+static constexpr uint8_t goods_type_sorcery = 5;
+static constexpr uint8_t goods_type_spirit_summon_lesser = 7;
+static constexpr uint8_t goods_type_spirit_summon_greater = 8;
+static constexpr uint8_t goods_type_wondrous_physick = 9;
+static constexpr uint8_t goods_type_wondrous_physick_tear = 10;
+static constexpr uint8_t goods_type_regenerative_material = 11;
+static constexpr uint8_t goods_type_info_item = 12;
+static constexpr uint8_t goods_type_reinforcement_material = 14;
+static constexpr uint8_t goods_type_great_rune = 15;
+static constexpr uint8_t goods_type_incantation = 16;
+static constexpr uint8_t goods_type_self_buff_sorcery = 17;
+static constexpr uint8_t goods_type_self_buff_incantation = 18;
 // Convergence has a unique goods type for consumable runes and remembrances
-static constexpr unsigned char goods_type_convergence_rune = 100;
-static constexpr unsigned char goods_type_convergence_remembrance = 101;
+static constexpr uint8_t goods_type_convergence_rune = 100;
+static constexpr uint8_t goods_type_convergence_remembrance = 101;
 
-static constexpr unsigned char goods_sort_group_tutorial = 20;
-static constexpr unsigned char goods_sort_group_gesture = 250;
+static constexpr uint8_t goods_sort_group_tutorial = 20;
+static constexpr uint8_t goods_sort_group_gesture = 250;
 
 /*
  * Items that are automatically given to the player in certain scenarios, and should not be
  * purchasable
  */
-static const std::set<long long> excluded_goods = {
+static const std::set<int64_t> excluded_goods = {
     107, // Phantom Bloody Finger
     113, // Phantom Bloody Finger
     114, // Phantom Recusant Finger
@@ -102,13 +103,13 @@ static const std::set<long long> excluded_goods = {
     1066, 1067, 1068, 1069, 1070, 1071, 1072, 1073, 1074, 1075};
 
 /* Additional items that don't have the unauthorized "[ERROR]" prefix, but are unobtainable */
-static const std::set<long long> cut_content_goods = {
+static const std::set<int64_t> cut_content_goods = {
     8860,    // Erdtree Prayerbook
     8861,    // Erdtree Codex
     2008023, // Keep Wall Key
 };
 
-static const std::set<long long> cut_content_protectors = {
+static const std::set<int64_t> cut_content_protectors = {
     610000, // Ragged Hat
     610100, // Ragged Armor
     610200, // Ragged Gloves
@@ -122,25 +123,25 @@ static const std::set<long long> cut_content_protectors = {
     956100, // [ERR] Roundtable Armor (Altered)
 };
 
-static const std::set<long long> cut_content_weapons = {
+static const std::set<int64_t> cut_content_weapons = {
     33290000, // [ERR] ST Staff
 };
 
-static const std::set<long long> cut_content_accessories = {
+static const std::set<int64_t> cut_content_accessories = {
     3100, // Bluntstone Glintstone Blade
     6120, // Invisible Trick Mirror
     6121, // Unbeatable Trick Mirror
 };
 
-static constexpr unsigned int kale_alive_flag_id = 4700;
-static constexpr unsigned int kale_hostile_flag_id = 4701;
-static constexpr unsigned int kale_dead_flag_id = 4703;
+static constexpr uint32_t kale_alive_flag_id = 4700;
+static constexpr uint32_t kale_hostile_flag_id = 4701;
+static constexpr uint32_t kale_dead_flag_id = 4703;
 
 static from::CS::GameDataMan **game_data_man_addr;
 
 struct shop
 {
-    long long id;
+    int64_t id;
     std::vector<from::paramdef::SHOP_LINEUP_PARAM> lineups;
 };
 
@@ -170,15 +171,15 @@ static std::array<shop, 22> mod_shops = {
 };
 
 // Map of ReinforceParamWeapon IDs to the maximum possible level of weapons using that upgrade path
-static std::map<short, unsigned char> max_level_by_reinforce_type_id;
+static std::map<int16_t, uint8_t> max_level_by_reinforce_type_id;
 
 // Goods that shouldn't be allowed in the storage box, because acquiring a second copy can break
 // things
-static std::set<unsigned int> no_repository_item_ids;
+static std::set<uint32_t> no_repository_item_ids;
 
 static bool is_shop_open = false;
 
-static shop *get_mod_shop(int shop_lineup_id)
+static shop *get_mod_shop(int32_t shop_lineup_id)
 {
     for (auto &shop : mod_shops)
     {
@@ -192,7 +193,7 @@ static shop *get_mod_shop(int shop_lineup_id)
 }
 
 static from::find_shop_menu_result *(*solo_param_repository_lookup_shop_menu)(
-    from::find_shop_menu_result *result, unsigned char shop_type, int begin_id, int end_id);
+    from::find_shop_menu_result *result, uint8_t shop_type, int32_t begin_id, int32_t end_id);
 
 /**
  * Hook for SoloParamRepositoryImp::LookupShopMenu()
@@ -200,7 +201,7 @@ static from::find_shop_menu_result *(*solo_param_repository_lookup_shop_menu)(
  * Return a shop menu for the Glorious Merchant shop, or fall back to the vanilla shop menus.
  */
 static from::find_shop_menu_result *solo_param_repository_lookup_shop_menu_detour(
-    from::find_shop_menu_result *result, unsigned char shop_type, int begin_id, int end_id)
+    from::find_shop_menu_result *result, uint8_t shop_type, int32_t begin_id, int32_t end_id)
 {
     for (auto &shop : mod_shops)
     {
@@ -216,8 +217,8 @@ static from::find_shop_menu_result *solo_param_repository_lookup_shop_menu_detou
     return solo_param_repository_lookup_shop_menu(result, shop_type, begin_id, end_id);
 }
 
-static void (*solo_param_repository_lookup_shop_lineup)(from::find_shop_menu_result *,
-                                                        unsigned char, int id);
+static void (*solo_param_repository_lookup_shop_lineup)(from::find_shop_menu_result *, uint8_t,
+                                                        int32_t id);
 
 /**
  * Hook for SoloParamRepositoryImp::LookupShopLineup()
@@ -226,7 +227,7 @@ static void (*solo_param_repository_lookup_shop_lineup)(from::find_shop_menu_res
  * lineups.
  */
 static void solo_param_repository_lookup_shop_lineup_detour(from::find_shop_menu_result *result,
-                                                            unsigned char shop_type, int id)
+                                                            uint8_t shop_type, int32_t id)
 {
     auto shop = get_mod_shop(id);
     if (shop && id < shop->id + shop->lineups.size())
@@ -240,12 +241,12 @@ static void solo_param_repository_lookup_shop_lineup_detour(from::find_shop_menu
     solo_param_repository_lookup_shop_lineup(result, shop_type, id);
 }
 
-static void (*open_regular_shop)(void *, long long, long long);
+static void (*open_regular_shop)(void *, int64_t, int64_t);
 
 /**
  * Hook for OpenRegularShop()
  */
-static void open_regular_shop_detour(void *unk, long long begin_id, long long end_id)
+static void open_regular_shop_detour(void *unk, int64_t begin_id, int64_t end_id)
 {
     auto shop = get_mod_shop(begin_id);
 
@@ -265,9 +266,9 @@ static void open_regular_shop_detour(void *unk, long long begin_id, long long en
                 auto weapon_id = lineup.equipId - lineup.equipId % 100;
                 auto weapon = equip_param_weapon[weapon_id];
 
-                auto reinforce_level =
-                    (int)std::floor((max_reinforce_level + 0.5) *
-                                    max_level_by_reinforce_type_id[weapon.reinforceTypeId] / 25);
+                auto reinforce_level = (int32_t)std::floor(
+                    (max_reinforce_level + 0.5) *
+                    max_level_by_reinforce_type_id[weapon.reinforceTypeId] / 25);
 
                 lineup.equipId = weapon_id + reinforce_level;
             }
@@ -286,7 +287,7 @@ static void open_regular_shop_detour(void *unk, long long begin_id, long long en
     }
 }
 
-static int (*get_sell_value)(unsigned int *);
+static int32_t (*get_sell_value)(uint32_t *);
 
 /**
  * Hook for GetSellValue()
@@ -295,7 +296,7 @@ static int (*get_sell_value)(unsigned int *);
  * touch the param fields because this would prevent items from being sold back to merchants, and
  * because Seamless Co-op doesn't allow matchmaking with modified params.
  */
-static int get_sell_value_detour(unsigned int *item_id)
+static int32_t get_sell_value_detour(uint32_t *item_id)
 {
     if (is_shop_open)
     {
@@ -305,7 +306,7 @@ static int get_sell_value_detour(unsigned int *item_id)
     return get_sell_value(item_id);
 }
 
-static unsigned long long (*get_max_repository_num)(unsigned int *);
+static uint64_t (*get_max_repository_num)(uint32_t *);
 
 /**
  * Hook for GetMaxRepositoryNum()
@@ -313,7 +314,7 @@ static unsigned long long (*get_max_repository_num)(unsigned int *);
  * Overrides certain items to prevent them from being placed in the storage box, because they
  * should be limited to 1 total purchased to avoid breaking certain flags.
  */
-static unsigned long long get_max_repository_num_detour(unsigned int *item_id)
+static uint64_t get_max_repository_num_detour(uint32_t *item_id)
 {
     if (is_shop_open && no_repository_item_ids.contains(*item_id))
     {
@@ -323,14 +324,14 @@ static unsigned long long get_max_repository_num_detour(unsigned int *item_id)
     return get_max_repository_num(item_id);
 }
 
-static unsigned int (*get_event_flag)(void *, unsigned int);
+static uint32_t (*get_event_flag)(void *, uint32_t);
 
 /**
  * Hook for CS::CSFD4VirtualMemoryFlag::GetEventFlag()
  *
  * Make Kalé always alive and non-hostile
  */
-static unsigned int get_event_flag_detour(void *self, unsigned int flag_id)
+static uint32_t get_event_flag_detour(void *self, uint32_t flag_id)
 {
     switch (flag_id)
     {
@@ -372,12 +373,12 @@ void ermerchant::setup_shops()
 
     // Look up event flags set when acquiring items like maps and cookbooks. Simply possessing
     // these items doesn't actually unlock anything, an event flag must also be set.
-    std::map<int, unsigned int> goods_flags;
-    std::map<int, unsigned int> gems_flags;
+    std::map<int32_t, uint32_t> goods_flags;
+    std::map<int32_t, uint32_t> gems_flags;
 
     // Look up goods IDs that are only used for replacement text in shops. These aren't actual
     // obtainable items.
-    std::set<long long> dummy_goods_ids;
+    std::set<int64_t> dummy_goods_ids;
 
     for (auto param : {L"ItemLotParam_map", L"ItemLotParam_enemy"})
     {
@@ -421,7 +422,7 @@ void ermerchant::setup_shops()
 
     // Count of the number of times each flag prefix is used, to help identify flags that are usable
     // as stock counters.
-    std::map<unsigned int, int> goods_flag_counts;
+    std::map<uint32_t, int32_t> goods_flag_counts;
     for (auto [_, flag] : goods_flags)
         goods_flag_counts[flag - flag % 10]++;
 
@@ -486,7 +487,7 @@ void ermerchant::setup_shops()
             lineups = &weapon_lineups;
         }
 
-        lineups->push_back({.equipId = (int)id, .equipType = equip_type_weapon});
+        lineups->push_back({.equipId = (int32_t)id, .equipType = equip_type_weapon});
     }
 
     for (auto [id, row] :
@@ -536,7 +537,7 @@ void ermerchant::setup_shops()
             lineups = &armor_lineups;
         }
 
-        lineups->push_back({.equipId = (int)id, .equipType = equip_type_protector});
+        lineups->push_back({.equipId = (int32_t)id, .equipType = equip_type_protector});
     }
 
     for (auto [id, row] :
@@ -570,7 +571,7 @@ void ermerchant::setup_shops()
             lineups = &talisman_lineups;
         }
 
-        lineups->push_back({.equipId = (int)id, .equipType = equip_type_accessory});
+        lineups->push_back({.equipId = (int32_t)id, .equipType = equip_type_accessory});
     }
 
     for (auto [id, row] :
@@ -689,7 +690,7 @@ void ermerchant::setup_shops()
         if (lineups)
         {
             auto event_flag = goods_flags[id];
-            short sell_quantity = -1;
+            int16_t sell_quantity = -1;
 
             // Check for maps, crafting kit, key items, etc. that shouldn't be allowed to have
             // duplicates. Buying extra copies of key items can unset event flags and break things
@@ -710,7 +711,7 @@ void ermerchant::setup_shops()
             }
 
             lineups->push_back({
-                .equipId = (int)id,
+                .equipId = (int32_t)id,
                 .eventFlag_forStock = event_flag,
                 .sellQuantity = sell_quantity,
                 .equipType = equip_type_goods,
@@ -747,7 +748,7 @@ void ermerchant::setup_shops()
 
         auto event_flag_it = gems_flags.find(id);
         lineups->push_back({
-            .equipId = (int)id,
+            .equipId = (int32_t)id,
             .eventFlag_forStock = event_flag_it == gems_flags.end() ? 0 : event_flag_it->second,
             .equipType = equip_type_gem,
         });
